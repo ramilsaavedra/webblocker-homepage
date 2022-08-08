1eda69066abf347abb0b70f02baad0d58234a1e7
Ramil Saavedra <ramilsmcl@gmail.com>
2022-08-09 07:50:46 +0800

    feat: open graph for facebook

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,6 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Web Blocker</title>
-        <meta name='description' content='Block unlimited website for free' />
-        <link rel='icon' href='/favicon.ico' />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
       </Head>
       <Header />
       <main className={styles.main}>
